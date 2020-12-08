4aa4d3e0171ac6f6dd519a6291d820786d0f6bfc
bee-olan <bee.olan@mail.ru>
2020-12-08 19:04:24 +0300

    add UseCase/SignUp/Request/Handler.php

---
+++ src/Model/User/UseCase/SignUp/Request/Handler.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Model\User\UseCase\SignUp\Request;
+
+use App\Model\Flusher;
+use App\Model\User\Entity\User\Email;
+use App\Model\User\Entity\User\Id;
+use App\Model\User\Entity\User\User;
+use App\Model\User\Entity\User\UserRepository;
+use App\Model\User\Service\PasswordHasher;
+
+class Handler
+{
+    private $users;
+    private $hasher;
+    private $flusher;
+
+    public function __construct(
+        UserRepository $users,
+        PasswordHasher $hasher,
+        Flusher $flusher
+    )
+    {
+        $this->users = $users;
+        $this->hasher = $hasher;
+        $this->flusher = $flusher;
+    }
+
+    public function handle(Command $command): void
+    {
+        $email = new Email($command->email);
+
+        if ($this->users->hasByEmail($email)) {
+            throw new \DomainException('Такой пользователь уже есть');
+        }
+
+        $user = User::signUpByEmail(
+            Id::next(),
+            new \DateTimeImmutable(),
+            $email,
+            $this->hasher->hash($command->password)
+        );
+
+        $this->users->add($user);
+
+        $this->flusher->flush();
+
+    }
+}
