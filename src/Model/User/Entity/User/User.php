e88f1719a94c15b1f4e08e68e311b145f56136ab
bee-olan <bee.olan@mail.ru>
2020-12-08 19:53:05 +0300

    add User Email Id UserRepository

---
+++ src/Model/User/Entity/User/User.php
@@ -0,0 +1,149 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Model\User\Entity\User;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\HasLifecycleCallbacks
+ * @ORM\Table(name="user_users", uniqueConstraints={
+ *     @ORM\UniqueConstraint(columns={"email"}),
+ *     @ORM\UniqueConstraint(columns={"reset_token_token"})
+ * })
+ */
+class User
+{
+ 
+    /**
+     * @ORM\Column(type="user_user_id")
+     * @ORM\Id
+     */
+    private $id;
+	
+    /**
+     * @var \DateTimeImmutable
+     * @ORM\Column(type="datetime_immutable")
+     */
+    private $date;
+
+    /**
+     * @var Email|null
+     * @ORM\Column(type="user_user_email", nullable=true)
+     */
+    private $email;
+
+    /**
+     * @var string|null
+     * @ORM\Column(type="string", name="password_hash", nullable=true)
+     */
+    private $passwordHash;
+
+
+    private function __construct(Id $id, \DateTimeImmutable $date)
+    {
+        $this->id = $id;
+        $this->date = $date;
+   
+    }
+
+    public static function create(Id $id, \DateTimeImmutable $date, Name $name, Email $email, string $hash): self
+    {
+        $user = new self($id, $date, $name);
+        $user->email = $email;
+        $user->passwordHash = $hash;
+        $user->status = self::STATUS_ACTIVE;
+        return $user;
+    }
+
+    public static function signUpByEmail(Id $id, \DateTimeImmutable $date, Name $name, Email $email, string $hash, string $token): self
+    {
+        $user = new self($id, $date, $name);
+        $user->email = $email;
+        $user->passwordHash = $hash;
+        $user->confirmToken = $token;
+        $user->status = self::STATUS_WAIT;
+        return $user;
+    }
+
+
+    public function requestPasswordReset(ResetToken $token, \DateTimeImmutable $date): void
+    {
+        if (!$this->isActive()) {
+            throw new \DomainException('User is not active.');
+        }
+        if (!$this->email) {
+            throw new \DomainException('Email is not specified.');
+        }
+        if ($this->resetToken && !$this->resetToken->isExpiredTo($date)) {
+            throw new \DomainException('Resetting is already requested.');
+        }
+        $this->resetToken = $token;
+    }
+
+    public function passwordReset(\DateTimeImmutable $date, string $hash): void
+    {
+        if (!$this->resetToken) {
+            throw new \DomainException('Resetting is not requested.');
+        }
+        if ($this->resetToken->isExpiredTo($date)) {
+            throw new \DomainException('Reset token is expired.');
+        }
+        $this->passwordHash = $hash;
+        $this->resetToken = null;
+    }
+
+
+    public function edit(Email $email, Name $name): void
+    {
+        $this->name = $name;
+        $this->email = $email;
+    }
+
+    public function block(): void
+    {
+        if ($this->isBlocked()) {
+            throw new \DomainException('User is already blocked.');
+        }
+        $this->status = self::STATUS_BLOCKED;
+    }
+
+    public function isWait(): bool
+    {
+         return $this->status === self::STATUS_WAIT;
+    }
+
+    public function isActive(): bool
+    {
+         return $this->status === self::STATUS_ACTIVE;
+    }
+
+    public function isBlocked(): bool
+    {
+        return $this->status === self::STATUS_BLOCKED;
+    }
+
+    public function getId(): Id
+    {
+        return $this->id;
+    }
+
+    public function getDate(): \DateTimeImmutable
+    {
+        return $this->date;
+    }
+
+    public function getEmail(): ?Email
+    {
+        return $this->email;
+    }
+
+    public function getPasswordHash(): ?string
+    {
+        return $this->passwordHash;
+    }
+
+}
---
+++ src/Model/User/Entity/User/UserRepository.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Model\User\Entity\User;
+
+use App\Model\EntityNotFoundException;
+use Doctrine\ORM\EntityManagerInterface;
+
+class UserRepository
+{
+    private $em;
+    /**
+     * @var \Doctrine\ORM\EntityRepository
+     */
+    private $repo;
+
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+        $this->repo = $em->getRepository(User::class);
+    }
+
+
+    public function get(Id $id): User
+    {
+        /** @var User $user */
+        if (!$user = $this->repo->find($id->getValue())) {
+            throw new EntityNotFoundException('User is not found.');
+        }
+        return $user;
+    }
+
+    public function hasByEmail(Email $email): bool
+    {
+        return $this->repo->createQueryBuilder('t')
+                ->select('COUNT(t.id)')
+                ->andWhere('t.email = :email')
+                ->setParameter(':email', $email->getValue())
+                ->getQuery()->getSingleScalarResult() > 0;
+    }
+
+    public function add(User $user): void
+    {
+        $this->em->persist($user);
+    }
+}
